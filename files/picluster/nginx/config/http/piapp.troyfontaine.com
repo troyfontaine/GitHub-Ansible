90c4dc8489850903d69b1e708c2b45e406d509ce
Troy Fontaine <tfontaine@troyfontaine.com>
2015-08-01 19:49:08 -0500

    Overhaul of playbooks

---
+++ files/picluster/nginx/config/http/piapp.troyfontaine.com
@@ -0,0 +1,13 @@
+# Pi HTTP server
+
+server {
+	listen 80;
+	#server_name localhost;
+
+	root /var/www/html;
+	index index.html index.htm;
+
+	location / {
+		try_files $uri $uri/ =404;
+	}
+ }
---
+++ roles/armv7_common/files/initialstate/ctl-r2001.py
@@ -0,0 +1,32 @@
+#####
+# This is a custom 'getting started' script, made with care for tfontaine@troyfontaine.com.
+# If you have any questions, please email us! support@initialstate.com
+#####
+
+# Import the ISStreamer module
+from ISStreamer.Streamer import Streamer
+# Import time for delays
+import time
+
+# Streamer constructor, this will create a bucket called Python Stream Example
+# you'll be able to see this name in your list of logs on initialstate.com
+# your access_key is a secret and is specific to you, don't share it!
+streamer = Streamer(bucket_name="CTL-R2001", bucket_key="default_configv1", access_key="sm28U3rT9LdlrJofIMw5WVbOmFxN6VFy")
+
+# example data logging
+streamer.log("My Messages", "Stream Starting")
+for num in range(1, 20):
+	time.sleep(0.1)
+	streamer.log("My Numbers", num)
+	if num%2 == 0:
+		streamer.log("My Booleans", False)
+	else: 
+		streamer.log("My Booleans", True)
+	if num%3 == 0:
+		streamer.log("My Events", "pop")
+	if num%10 == 0:
+		streamer.log("My Messages", "Stream Half Done")
+streamer.log("My Messages", "Stream Done")
+
+# Once you're finished, close the stream to properly dispose
+streamer.close()
